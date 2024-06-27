a33b34963a45d0320d2e86b9e8e533e62bfd6ccb
Olutunmibi Iseoluwa Olatunde <125552420+Olatunde16@users.noreply.github.com>
2024-06-27 19:41:10 +0100

    Completed Advanced Task

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,12 +1,12 @@
 /** 
  * Import the Car class from 10-car.js
  */
-import Car from './10-car.js';
+import Car from './10-car';
 
 export default class EVCar extends Car {
   constructor(brand, motor, color, range) {
     super(brand, motor, color);
-    this._range = range;
+    this.range = range;
   }
 
   get range() {
