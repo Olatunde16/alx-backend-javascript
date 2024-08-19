1a4030e8efe6eacd8d227600f49e44a6bd8c6091
Olutunmibi Iseoluwa <olatundeolutunmibi@gmail.com>
2024-08-19 01:24:26 -0700

    Ongoing

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,11 @@
+/**
+ * Displays a message on the STDOUT.
+ * @param {String} message The message to display.
+ * @author Olutunmibi Iseoluwa <https://github.com/Olatunde16>
+ */
+
+function displayMessage(message) {
+	console.log(message);
+}
+
+module.exports = displayMessage;
