5dc8d58b3f4e3747f6c6076afa695eec10a15cc2
Olutunmibi Iseoluwa <olatundeolutunmibi@gmail.com>
2024-08-19 01:42:47 -0700

    Tasks Successfully done

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 /**
  * Counts the students in a CSV data file.
  * @param {String} dataPath The path to the CSV data file.
- * @author Bezaleel Olakunori <https://github.com/B3zaleel>
+ * @author Olutunmibi Iseoluwa <https://github.com/Olatunde16>
  */
 const countStudents = (dataPath) => {
   if (!fs.existsSync(dataPath)) {
